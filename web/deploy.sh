a08be344f1fe3f89626bd0bff7601b29e5bb884c
Miko0000 <tarmikodeveloper@gmail.com>
2025-05-26 18:06:35 +0700

    Changed name for convenience

--- web/deploy.sh
+++ web/deploy.sh
@@ -2,7 +2,7 @@
 
 echo "DEPLOYING ] ";
 
-git subtree push --prefix result.minify origin static # > deploy.log 2>&1
+git subtree push --prefix web/result.minify origin static # > deploy.log 2>&1
 
 if test $? -eq 0; then
   printf "OK\n";
